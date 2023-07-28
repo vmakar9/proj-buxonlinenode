232cc5fccdb83b960cc35315a1f52824cc242b75
roman-zahoruiko-unkd <r.zahoruiko@unkd.com.ua>
2023-07-28 16:54:59 +0300

    upd VacancyAdmin view

--- backend/buxonline/admin.py
+++ backend/buxonline/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from buxonline.models import FirstLevelTaxonomy, SecondLevelTaxonomy, TechnologyItem, Country, Language, Vacancy
 
 
+class VacancyAdmin(admin.ModelAdmin):
+    list_filter = ('language', 'taxonomy',)
+    list_display = ('title', 'language', 'taxonomy', )
+    search_fields = ('title', 'text', )
+    readonly_fields = ('url', )
+
+
 admin.site.register(FirstLevelTaxonomy)
 admin.site.register(SecondLevelTaxonomy)
 admin.site.register(TechnologyItem)
 admin.site.register(Country)
 admin.site.register(Language)
-admin.site.register(Vacancy)
+admin.site.register(Vacancy, VacancyAdmin)

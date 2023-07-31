2be65f7a8eed986120cdc12e85071892ec3f3c32
roman-zahoruiko-unkd <r.zahoruiko@unkd.com.ua>
2023-07-31 16:45:14 +0300

    add VacancyMetaTranslatedSerializer, upd VacancySerializer meta field,

--- backend/buxonline/serializers.py
+++ backend/buxonline/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-from buxonline.models import Vacancy, FirstLevelTaxonomy, Language
+from buxonline.models import Vacancy, FirstLevelTaxonomy, Language, VacancyMetaTranslated
+
+
+class VacancyMetaTranslatedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VacancyMetaTranslated
+        fields = ('more', 'apply_for_job')
 
 
 class VacancySerializer(serializers.ModelSerializer):
+    meta = VacancyMetaTranslatedSerializer()
+
     class Meta:
         model = Vacancy
-        exclude = ('text_html', )
+        exclude = ('text_html', 'parent')
 
 
 class FirstLevelTaxonomySerializer(serializers.ModelSerializer):
@@ -18,5 +26,3 @@ class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Language
         fields = '__all__'
-
-

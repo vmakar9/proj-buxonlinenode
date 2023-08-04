27fdae530439dc99e5135bdd7e318c66c0eb80e2
roman-zahoruiko-unkd <r.zahoruiko@unkd.com.ua>
2023-08-04 12:02:57 +0300

    refactor vacancies_translation.py script

--- backend/buxonline/fixtures/vacancies_translation.py
+++ backend/buxonline/fixtures/vacancies_translation.py
@@ -30,35 +30,36 @@ def translate_to_one_lang_with_azure(api_key: str, from_lang: str, to_lang: str,
     return ''
 
 
-azure_translate_api_key = os.environ.get('AZURE_TRANSLATE_API_KEY')
-
-lang = Language.objects.filter(code_a2='ca').first()
-try:
-    vacancies = Vacancy.objects.filter(parent__isnull=True).order_by('pk')  # [3:]
-    for count, v in enumerate(vacancies):
-        title_translated = translate_to_one_lang_with_azure(
-            api_key=azure_translate_api_key,
-            from_lang=v.language.code_a2,
-            to_lang=lang.code_a2,
-            text=v.title,
-        )
-        sleep(0.3)
-        text_translated = translate_to_one_lang_with_azure(
-            api_key=azure_translate_api_key,
-            from_lang=v.language.code_a2,
-            to_lang=lang.code_a2,
-            text=v.text,
-        )
-        translated_v_meta, created = VacancyMetaTranslated.objects.get_or_create(language=lang)
-        translated_v = Vacancy.objects.create(
-            language=lang,
-            taxonomy=v.taxonomy,
-            meta=translated_v_meta,
-            title=title_translated,
-            text=text_translated,
-            parent=v,
-        )
-        sleep(0.3)
-        print(f'> {count+1}/{len(vacancies)} done! ({translated_v}).')
-except Exception as ex:
-    print('> translate error:', ex)
+if __name__ == '__main__':
+    azure_translate_api_key = os.environ.get('AZURE_TRANSLATE_API_KEY')
+    langs = Language.objects.filter(id__gt=13).exclude(id__in=[27, 30, 35])
+    for lang in langs:
+        print('>>> lang:', lang)
+        try:
+            vacancies = Vacancy.objects.filter(parent__isnull=True).order_by('pk')  # [207:]
+            for count, v in enumerate(vacancies):
+                title_translated = translate_to_one_lang_with_azure(
+                    api_key=azure_translate_api_key,
+                    from_lang=v.language.code_a2,
+                    to_lang=lang.code_a2,
+                    text=v.title,
+                )
+                sleep(0.3)
+                text_translated = translate_to_one_lang_with_azure(
+                    api_key=azure_translate_api_key,
+                    from_lang=v.language.code_a2,
+                    to_lang=lang.code_a2,
+                    text=v.text,
+                )
+                translated_v_meta, created = VacancyMetaTranslated.objects.get_or_create(language=lang)
+                translated_v = Vacancy.objects.create(
+                    language=lang,
+                    taxonomy=v.taxonomy,
+                    meta=translated_v_meta,
+                    title=title_translated,
+                    text=text_translated,
+                    parent=v,
+                )
+                print(f'> {count+1}/{len(vacancies)} done! ({translated_v}).')
+        except Exception as ex:
+            print('> translate error:', ex)

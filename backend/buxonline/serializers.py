edbd8aa695c4aaaa2a3c0cbfdd71d087a28baf7d
roman-zahoruiko-unkd <r.zahoruiko@unkd.com.ua>
2023-08-10 10:59:10 +0300

    add lang filtering to get_vacancies_count FirstLevelTaxonomyExtendedSerializer

--- backend/buxonline/serializers.py
+++ backend/buxonline/serializers.py
@@ -28,6 +28,10 @@ class FirstLevelTaxonomyExtendedSerializer(serializers.ModelSerializer):
     vacancies_count = serializers.SerializerMethodField()
 
     def get_vacancies_count(self, instance) -> int:
+        request = self.context.get('request')
+        lang = request.query_params.get('lang')
+        if lang:
+            return instance.vacancy_set.filter(language__code_a2=lang).count()
         return instance.vacancy_set.count()
 
     class Meta:

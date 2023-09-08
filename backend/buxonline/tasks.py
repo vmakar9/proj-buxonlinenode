1ea0d720be82a560c1128ddd6630cdd5a80ab230
roman-zahoruiko-unkd <r.zahoruiko@unkd.com.ua>
2023-09-08 13:37:43 +0300

    upd tasks.py

--- backend/buxonline/tasks.py
+++ backend/buxonline/tasks.py
@@ -5,7 +5,7 @@ from config.settings import AZURE_OPENAI_API_KEY, AZURE_TRANSLATE_API_KEY
 
 
 @shared_task
-def generate_vacancy_seo_data(language_name: str, start_from_vacancy_pk: int = None) -> [list, str]:
+def generate_vacancy_seo_data_for_lang(language_name: str, start_from_vacancy_pk: int = None) -> [list, str]:
     try:
         if not start_from_vacancy_pk:
             vacancies = Vacancy.objects.filter(language__name=language_name).order_by('pk')

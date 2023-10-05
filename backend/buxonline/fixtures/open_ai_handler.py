31b355619c7015b0b1069595d69e7963c85501df
yuhim01 <yuhim01@gmail.com>
2023-10-05 17:29:02 +0300

    Change keys

--- backend/buxonline/fixtures/open_ai_handler.py
+++ backend/buxonline/fixtures/open_ai_handler.py
@@ -123,14 +123,14 @@ def generate_ads_data(prompt, api_key: str, target_element_len: int = None) -> l
     # 'https://chat-gpt-4-usa.openai.azure.com/'
     openai.api_base = 'https://oai.hconeai.com'
     openai.api_version = "2023-03-15-preview"  # 2023-05-15
-    openai.api_key = api_key
+    openai.api_key = os.environ.get('HELICONE_API_KEY')
     time.sleep(3)
 
     for i in range(20):
         try:
             raw_answer = openai.ChatCompletion.create(
                 engine='gpt-35', messages=prompt, headers={
-                    "Helicone-Auth": f"Bearer {os.environ.get('HELICONE_API_KEY')}",
+                    "Helicone-Auth": f"Bearer {api_key}",
                     "Helicone-OpenAI-Api-Base": 'https://bablo.openai.azure.com/',
                 })  # "gpt-4-32k"
 

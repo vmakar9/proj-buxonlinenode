b8a2e6ad7b0e07af79161ea2c4c97f322f6467d1
Oleh Papka <olegpapka2@gmail.com>
2023-11-22 16:05:11 +0200

    Fix: AZURE_OPENAI_API_KEY changed to OPENAI_KEY

--- backend/buxonline/fixtures/open_ai_handler.py
+++ backend/buxonline/fixtures/open_ai_handler.py
@@ -130,8 +130,8 @@ def generate_ads_data(prompt, api_key: str, target_element_len: int = None) -> l
     for i in range(20):
         try:
             raw_answer = openai.ChatCompletion.create(
-                engine='gpt-3.5-turbo', messages=prompt, headers={
-                    # "Helicone-Auth": f"Bearer {os.environ.get('HELICONE_API_KEY')}",
+                model='gpt-3.5-turbo', messages=prompt, headers={
+                    "Helicone-Auth": f"Bearer {os.environ.get('HELICONE_API_KEY')}",
                     # "Helicone-OpenAI-Api-Base": 'https://bablo.openai.azure.com/',
                 })  # "gpt-4-32k"
 

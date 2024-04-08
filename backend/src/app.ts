4c6faeb4bbd2629026dce7beacb6fc5e415d0253
vmakar9 <zmerin212@gmail.com>
2024-04-08 13:35:50 +0300

    Init

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import cors from "cors";
 import express from "express";
+import { Request, Response } from "express";
 import fileUploader from "express-fileupload";
 import * as mongoose from "mongoose";
 
 import { configs } from "./configs/configs";
+import { ApiError } from "./erorr/api.error";
 import { adminRouter } from "./routers/admin.router";
 import { authRouter } from "./routers/auth.router";
 import { candidateRouter } from "./routers/candidate.router";
@@ -33,6 +35,13 @@ app.use("/company", companyRouter);
 app.use("/admin", adminRouter);
 app.use("/vacancy", vacancyRouter);
 app.use("/vacancy-feedback", feedbackonvacancyRouter);
+
+app.use("*", (err: ApiError, req: Request, res: Response) => {
+  return res.status(err?.status || 500).json({
+    message: err?.message,
+    status: err?.status,
+  });
+});
 app.listen(configs.PORT, async () => {
   await mongoose.connect(configs.DB_URL);
 

0d0b902dd860a697acc3b1fd4d4c159c3f0117c7
vmakar9 <zmerin212@gmail.com>
2024-04-15 15:29:46 +0300

    Index

--- backend/src/controllers/hello.controller.ts
+++ backend/src/controllers/hello.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-class HelloController {
+export class HelloController {
   public async hello(req: Request, res: Response, next: NextFunction) {
     try {
       return res.sendStatus(200).json("Hello World");
